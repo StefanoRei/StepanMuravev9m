a0d83dc7fc6f4c0006d4c898145ee238f2e6237f
StefanoRei <148196524+StefanoRei@users.noreply.github.com>
2024-02-13 11:01:44 +0300

    Update kr13.02_part2.py

--- kr13.02_part2.py
+++ kr13.02_part2.py
@@ -1,2 +1,5 @@
-def remove_values_from_list(lst, val):
+def remove_values():
+    lst = list(map(int, input().split()))
+    val = int(input())
     return [x for x in lst if x != val]
+print(remove_values())
